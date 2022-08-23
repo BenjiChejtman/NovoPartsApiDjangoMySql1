cb6ed57d3fb395b882613d49948328fa7ed97f78
BenjiChejtman <46895375@est.ort.edu.ar>
2022-08-23 15:49:47 -0300

    Minor Changes

--- NovoAPI/NovoApi_APP/views.py
+++ NovoAPI/NovoApi_APP/views.py
@@ -1,18 +1,32 @@
 #from django.shortcuts import render
-from django.views import view
+from django.http.response import JsonResponse
+from django.views import View
+from .models import novo
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 class NovoView(View):
+    
+    @method_decorator(csrf_exempt) #nos exonera de esta restriccion, para que no salte esta restriccion
+    def  dispatch(self, request, *args, **kwargs) #despachar o enviar y se ejecuta cada vez que hacemos una peticion
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        novop = list(company.objects.values())
+        novop = list(novo.objects.values())
         if len(novop) > 0:
-            datos = {'message': "Success!"}
+            datos = {'message': "Success!" + novo.objects.values}
         else:
-            datos = {'message': "Novo Parts not found"}
+            datos = {'message': "Info not found"}
         return JsonResponse(datos)
+
+
     def post(self, request):
-        pass
+        datos = {'message':'Success'}
+        return JsonResponse(datos)
+
     def put(self, request):
         pass
+
     def delete(self, request):
         pass
